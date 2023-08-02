44edea238343c245bd31d5d39619183a40073123
Loli-Eternally <40571685+Loli-Eternally@users.noreply.github.com>
2023-08-02 17:35:32 +0800

    Testing

--- main.py
+++ main.py
@@ -19,7 +19,27 @@ def dipole():
 
     return fig1, fig2
 
+
+def ridge_waveguide():
+    omega = 2 * np.pi * 200e12
+    dl = 0.01  # grid size (units of L0, which defaults to 1e-6)
+    eps_r = np.ones((600, 200))  # relative permittivity
+    eps_r[:, 90:110] = 12.25  # set waveguide region
+    NPML = [15, 15]  # number of pml grid points on x and y borders
+
+    simulation = Simulation(omega, eps_r, dl, NPML, 'Ez')
+    simulation.add_mode(3.5, 'x', [20, 100], 60, scale=10)
+    simulation.setup_modes()
+    simulation.solve_fields()
+    print('input power of {} in W/L0'.format(simulation.W_in))
+
+    fig1 = simulation.plt_re(outline=True, cbar=False)
+    fig2 = simulation.plt_abs(outline=True, cbar=False)
+
+    return fig1, fig2
+
+
 # 按间距中的绿色按钮以运行脚本。
 if __name__ == '__main__':
-    dipole()
+    ridge_waveguide()
     plt.show()
