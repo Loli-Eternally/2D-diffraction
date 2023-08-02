049708ec3481a188847d5cd1c7460ac8d3f04d23
Loli-Eternally <40571685+Loli-Eternally@users.noreply.github.com>
2023-08-02 12:38:56 +0800

    Testing

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import numpy as np
+
 from DiffractiX import Simulation
+
 
 def dipole():
     omega = 2*np.pi*200e12
@@ -9,7 +11,7 @@ def dipole():
 
     simulation = Simulation(omega, eps_r, dl, NPML, 'Ez')
     simulation.src[100, 100] = 1
-    simulation.solve_fields()
+    simulation.solve_fields(
 
     simulation.plt_abs(outline=False, cbar=True);
     simulation.plt_re(outline=False, cbar=True);
